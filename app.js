e5dd13f593fed40a86918a325eab205061c224f9
BHazlerig <benhazlerig@gmail.com>
2014-03-30 09:11:38 -0400

    Added form validation

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ $(document).ready(function() {
 		   $('.btn').trigger('click');
 	    }	   
 	});
-	    $('.btn').click(function(){
-		        var fizzNumber = $('.input-number').val();  	
-			    for(i=fizzNumber; i < 101; i++) {
-					if( ( i % 15 == 0) ){
-					$('.fizz-space').append('<p>fizz buzz</p>');
-					}
-					else if( i%3 == 0){
-					$('.fizz-space').append('<p>fizz</p>');
-					}
-					else if( i%5 == 0) {
-					$('.fizz-space').append('<p>buzz</p>');
-					} 
-					else {
-					$('.fizz-space').append('<p>' + i + '</p>');
-				    } 			    
+	$('.btn').click(function(){
+	    var fizzNumber = $('.input-number').val();  	
+	    if ( fizzNumber > 0 && fizzNumber < 101 ) {
+		    for(i=fizzNumber; i < 101; i++) {
+				if( (i % 15 == 0) ){
+				$('.fizz-space').append('<p>fizz buzz</p>');
 				}
-		}); 
-   
+				else if(i % 3 == 0){
+				$('.fizz-space').append('<p>fizz</p>');
+				}
+				else if(i % 5 == 0) {
+				$('.fizz-space').append('<p>buzz</p>');
+				} 
+				else {
+				$('.fizz-space').append('<p>' + i + '</p>');
+			    } 			    
+			}
+		}
+		else {
+			alert("Please enter a number between 1 and 100");
+		}	
+	}); 
+
 });
